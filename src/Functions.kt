af070dc570c7ebe80ddb11776978f478dac33ba4
adityabangun <artadityabs10@gmail.com>
2025-09-18 14:00:54 +0700

    update functions

--- src/Functions.kt
+++ src/Functions.kt
@@ -13,4 +13,16 @@ fun main() {
 
     printMessageWithPrefix(prefix = "Log", message = "Hello")
     // [Log] Hello
+
+    val numbers = listOf(1, -2, 3, -4, 5, -6)
+
+    val positives = numbers.filter ({ x -> x > 0 })
+
+    val isNegative = { x: Int -> x < 0 }
+    val negatives = numbers.filter(isNegative)
+
+    println(positives)
+// [1, 3, 5]
+    println(negatives)
+// [-2, -4, -6]
 }
